c112a15030407e0c42024beb31df1e7f17bb13ce
siddhant <siddhaant.sharma@gmail.com>
2026-09-22 13:18:50 +0530

    Sort the imports

--- papers/cores/scripts/shared_mechanisms.py
+++ papers/cores/scripts/shared_mechanisms.py
@@ -11,7 +11,6 @@ Reports how many mechanisms each network has, how many it shares with each other
 network, and how many are common to all five.
 """
 import collections
-import itertools
 
 from _common import FOOD, NETS
 
